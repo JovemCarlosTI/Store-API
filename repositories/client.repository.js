1aee303c737b1a737cb92355d0ba4a66d444cdab
jovemcarlosti <carlosandre116@outlook.com>
2023-04-22 17:35:22 -0300

    insertClient

--- repositories/client.repository.js
+++ repositories/client.repository.js
@@ -1,5 +1,14 @@
-async function insertClient(client) {
+import { connect } from "./repository.js";
 
+async function insertClient(client) {
+    const conn = await connect();
+    const sql = "INSERT INTO clients (name, cpf, phone, email, address) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+
+    // O comando SQL não é concatenado pra evitar SQL Injection
+    const values = [client.name, client.cpf, client.phone, client.email, client.address];
+    const res = await conn.query(sql, values);
+
+    return res.rows[0];
 }
 
 export default {
